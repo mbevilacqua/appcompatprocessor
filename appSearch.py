267816b3d5a62d6e3f7ba53e0a6d84c79cf0e762
mbevilacqua <matias.bevilacqua@fireeye.com>
2017-09-28 12:52:26 +0200

    Fixed bug preventing search results from being saved to the output file when searching for known bad (no user supplied search term).

--- appSearch.py
+++ appSearch.py
@@ -463,9 +463,9 @@ class Consumer(multiprocessing.Process):
                                 else:
                                     logger.error("Producer/Consumer hit mismatch! (report bug please) - %s" % record.Search_Space)
                                 pass
-                            else:
-                                # We dump the data to the output file/s
-                                saveSearchData(record, self.searchType, regex_hit_name, text_file, markdown_file)
+
+                        # We dump the data to the output file/s
+                        saveSearchData(record, self.searchType, regex_hit_name, text_file, markdown_file)
 
                     # Update progress counter
                     with self.val.get_lock():
